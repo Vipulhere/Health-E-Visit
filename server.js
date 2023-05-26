1f371f10e2f5c0dcc82109da3fe620d4168cd293
Vipulhere <krvipul28@gmail.com>
2023-05-26 23:55:58 +0530

    Deployement successful

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import the necessary modules
 const express = require("express");
 const app = express();
+const path = require('path');
 require("dotenv").config();
 
 // Import the database configuration
